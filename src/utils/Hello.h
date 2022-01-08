05055e3646978a15f635a35fed26a18f74c541ae
hao ge <934622645@qq.com>
2022-01-08 20:15:54 +0800

    - basic file struct with cmake - add gtest support

---
+++ src/utils/Hello.h
@@ -0,0 +1,17 @@
+//
+// Created by 93462 on 2022/1/8.
+//
+
+#ifndef XJHCPP_HELLO_H
+#define XJHCPP_HELLO_H
+
+
+#include <string>
+
+class Hello {
+public:
+    static std::string sayhello();
+};
+
+
+#endif //XJHCPP_HELLO_H
