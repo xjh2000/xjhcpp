46c12e1c1f79865c114f13d93230d18dfae056ea
hao ge <934622645@qq.com>
2022-01-12 21:31:02 +0800

    - add target vector library

---
+++ src/gtest/VectorTest.cpp
@@ -0,0 +1,19 @@
+//
+// Created by 93462 on 1/12/2022.
+//
+
+#include "gtest/gtest.h"
+#include "Vector.h"
+
+
+namespace {
+
+    class VectorTest : public testing::Test {
+    };
+
+    TEST_F(VectorTest,initialized){
+        Vector<int> v;
+
+    }
+
+}
---
+++ src/vector/Vector.cpp
@@ -0,0 +1,6 @@
+//
+// Created by 93462 on 1/12/2022.
+//
+
+#include "Vector.h"
+
