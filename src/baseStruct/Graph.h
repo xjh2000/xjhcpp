0ec14a4d70bcb23c537d0df69cfa7e341bac83ab
xjh2000 <934622645@qq.com>
2022-02-18 22:52:06 +0800

    add  Dijkstra
    add
    - Dijkstra : shortest-path tree complex o(n*n)

--- src/baseStruct/Graph.h
+++ src/baseStruct/Graph.h
@@ -76,7 +76,7 @@ public:
 
     void prim ( int ); //最小支撑树Prim算法
 
-//    void dijkstra ( int ); //最短路径Dijkstra算法
+    void dijkstra ( int ); //最短路径Dijkstra算法
 
     template<typename PU>
     void pfs(int, PU); //优先级搜索框架
@@ -291,11 +291,28 @@ void Graph<Tv, Te>::prim(int s) {
 }
 
 template<typename Tv, typename Te>
+void Graph<Tv, Te>::dijkstra(int s) {
+    pfs(s,DijkstraPU<Tv,Te>());
+}
+
+template<typename Tv, typename Te>
 struct PrimPU {
     virtual void operator()(Graph<Tv, Te> *g, int uk, int v) {
         if (UNDISCOVERED == g->status(v)) {
             if (g->priority(v) > g->weight(uk, v)) {
                 g->priority(v) = g->weight(uk, v);
+                g->parent(v) = uk;
+            }
+        }
+    }
+};
+
+template<typename Tv, typename Te>
+struct DijkstraPU {
+    virtual void operator()(Graph<Tv, Te> *g, int uk, int v) {
+        if (UNDISCOVERED == g->status(v)) {
+            if (g->priority(v) > g->priority(uk) + g->weight(uk, v)) {
+                g->priority(v) = g->priority(uk) + g->weight(uk, v);
                 g->parent(v) = uk;
             }
         }
